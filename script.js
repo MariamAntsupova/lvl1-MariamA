03580e8cd1d3c380cb40fee009305480bed1653e
MariamAntsupova <mariam.antsupova.1@btu.edu.ge>
2021-05-28 17:34:09 +0400

    fake api

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const CardsList = document.querySelector(".cards");
-let limitNum = document.querySelector('.butval').value;
+let limitNum = document.querySelector('#butval');
+let fetchAPI = `https://fakestoreapi.com/products?limit=${limitNum.value}`;
+const datacols2 = document.querySelector(".datacols2").value;
+const datacols3 = document.querySelector(".datacols3").value;
+const datacols4 = document.querySelector(".datacols4").value;
+const datacols6 = document.querySelector(".datacols6").value;
 
-function request(){
-    fetch(` https://fakestoreapi.com/products?limit=${limitNum}`)
+
+
+limitNum.addEventListener('change', (event) => {
+    CardsList.innerHTML = ' ' ;
+    const fetchAPI = `https://fakestoreapi.com/products?limit=${event.target.value}`;
+    request(fetchAPI);
+  });
+
+
+
+function request(fetchAPI){
+    fetch(fetchAPI)
         .then(response => response.json())
         .then(json => 
             json.forEach(element => {
-                
                 CardsList.innerHTML += `
-                <div class="card  align-items-center text-center d-flex p-5 flex-fill" style="width: 18rem; border: none;">
+                <div class="card  align-items-center text-center d-flex p-5 flex-fill border-0 " >
 
                     <img src="${element.image}" class="card-img-top">
 
@@ -33,4 +47,27 @@ function request(){
             })
             
         )
+}
+function colsQuantity2(){
+    CardsList.className += `row row-cols-${datacols2}`;
+    console.log(CardsList)
+}
+
+function colsQuantity3(){
+    CardsList.className +=  `row row-cols-${datacols3}`;
+}
+function colsQuantity4(){
+    CardsList.className +=  `row row-cols-${datacols4}`;
+
+}
+function colsQuantity6(){
+    CardsList.className +=  `row row-cols-${datacols6}`;
+
+}
+function table(){
+    CardsList.className +=  `row row-cols-${datacols4}`;
+}
+
+function list(){
+    CardsList.className +=  `col col-md-auto`;
 }
